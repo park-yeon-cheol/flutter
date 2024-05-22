86be8f4656e53db4a0460950c8f64a1b47373877
park-yeon-cheol <qkr33291119@gmail.com>
2024-05-22 09:31:45 +0900

    navigator_example

---
+++ navigator/navigator_example.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SubPage Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {'/': (context) => FirstPage(),
+    '/second': (context) => SecondPage()},
+    );
+  }
+}
+
+class FirstPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _FirstPage();
+}
+
+class _FirstPage extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sub Page Main'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('첫 번째 페이지'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context)
+              .pushNamed('/second');
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Container(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: (){
+              Navigator.of(context).pop();
+            },
+            child: Text('돌아가기'),
+          ),
+        ),
+      ),
+    );
+  }
+}
