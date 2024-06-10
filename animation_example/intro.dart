d0a98fed02724ce6884aaf2053065327941d5b3c
park-yeon-cheol <qkr33291119@gmail.com>
2024-06-10 13:07:41 +0900

    intro

---
+++ animation_example/intro.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'saturnLoading.dart';
+import 'dart:async';
+import 'animation_example.dart';
+
+class IntroPage extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState() => _IntroPage();
+}
+
+class _IntroPage extends State<IntroPage>{
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  Future<Timer> loadData() async{
+    return Timer(Duration(seconds: 5), onDoneLoading);
+  }
+
+  onDoneLoading() async{
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => AnimationApp()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text('애니메이션 앱'),
+              SizedBox(height: 20,),
+              Saturnloading()
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
